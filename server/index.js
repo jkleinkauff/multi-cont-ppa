991b7c8abdde16380cb0b330b8f852d9463b5a09
Jhonatas Kleinkauff <kleinkauff@gmail.com>
2021-05-24 21:24:53 -0300

    adding other services

---
+++ server/index.js
@@ -0,0 +1,85 @@
+const keys = require("./keys");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const { Pool } = require("pg");
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+})
+
+pgClient.on("error", () => console.log("connection error"));
+
+pgClient.on("connect", (client) => {
+    client
+      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+      .catch((err) => console.error(err));
+});
+
+
+
+const redis = require("redis");
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+// redis recommends duplicate the client when subscribing
+const redisPublisher = redisClient.duplicate();
+
+//handle
+
+app.get("/", (req, res) => {
+    res.send("Hio");
+});
+
+app.get("/values/all", async (req, res) => {
+    const values = await pgClient.query("SELECT * from values");
+
+    res.send(values.rows)
+});
+
+
+app.get("/values/current", async (req, res) => {
+    redisClient.hgetall("values", (err, values) => {
+        res.send(values);
+    });
+});
+
+
+app.post("/values", async (req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40) {
+        return res.status(422).send("Index too high");
+    }
+
+    redisClient.hset("values", index, "nothing");
+    redisPublisher.publish("insert", index);
+    pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+    res.send({ working: true })
+});
+
+
+
+app.listen(5000, err => {
+    console.log("Listening on 5000")
+})
+
+
+
+
+
+
